e55502943317ed5435458f1a02e1b72c10fd700b
Om Londhe <oplondhe@gmail.com>
2023-04-11 10:32:07 +0530

    After solving Auth issue

--- api/auth/get.js
+++ api/auth/get.js
@@ -1,12 +1,12 @@
 const app = require("../../app");
 const User = require("../../db/models/user.model");
 
-app.post("/api/auth/check-if-email-exist", async (req, res) => {
+app.get("/api/auth/check-if-email-exist", async (req, res) => {
   const email = req.query.email;
 
   const user = await User.findOne({
     email,
   });
 
-  res.status(200).send({ emailExist: user !== undefined });
+  res.status(200).send({ emailExist: user !== null });
 });
